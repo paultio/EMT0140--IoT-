1296b5531fb5c7d8cf1d745d66293f10f7db6782
Paul Ööbik <paul@bugbox.ee>
2022-09-21 19:47:39 +0300

    week3

---
+++ ndl3/src/main.cpp
@@ -0,0 +1,16 @@
+// Includes global variables and librarys that the DHT uses
+#include <Arduino.h>
+
+void setup()
+{
+  // Initialize serial port and send message
+  Serial.begin(115200); // setting up serial connection parameter
+  Serial.println("Booting");
+
+ 
+}
+ 
+void loop()
+{
+
+}
